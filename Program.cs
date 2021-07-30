6c6eebcd5fc6a5b7b29f8472a358efd9911563d4
FIC <FIC@info.local>
2021-07-30 11:31:10 -0300

    Atividade c#

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace CsharpBianca
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            
+            Console.WriteLine("Qual é o seu nome?");
+            string nomePessoa = Console.ReadLine();
+
+            Console.WriteLine("Bem vinda, " + nomePessoa);
+        }
+    }
+}
