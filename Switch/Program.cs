1d86a0976713b3c6e735b6102ed352ce85146557
biancamarangoni <bchagas228@gmail.com>
2021-08-02 16:03:26 -0300

    Atividades

---
+++ Switch/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Switch
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insira sua idade!");
+            int idade = int.Parse(Console.ReadLine());
+
+            if (idade < 18)
+            {
+
+            }else if(idade > 18){
+                
+            }
+        }
+    }
+}
